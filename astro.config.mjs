ffdb0bcaa5012ff1cd2bdadbb0593ed81d403b12
inayath <inayath.corp@gmail.com>
2024-06-07 16:18:21 +0530

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'inayath-sh.github.io',
+  site: 'https://astrofy-template.netlify.app',
   integrations: [mdx(), sitemap(), tailwind()]
 });
