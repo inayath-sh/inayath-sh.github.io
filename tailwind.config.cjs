ebc86bbbee87f7ae42c5bb23a4f9e5421d7d40ff
inayath <inayath.corp@gmail.com>
2024-06-12 12:02:52 +0530

    menu color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,12 @@ module.exports = {
 	},
 	plugins: [require("@tailwindcss/typography"),require("daisyui")],
 	daisyui: {
-		themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+		themes:[{
+			autumn: {
+			  ...require("daisyui/src/theming/themes")["autumn"],
+			  neutral: "#91707b"
+			}}],
+		//themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
 		darkTheme: "dark", // name of one of the included themes for dark mode
 		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
 	  }
