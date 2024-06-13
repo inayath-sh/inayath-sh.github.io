79df5194d7f1ed454507bf244fb64db6ef8a9576
inayath <inayath.corp@gmail.com>
2024-06-13 14:00:57 +0530

    sidebar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,11 +6,6 @@ module.exports = {
 	},
 	plugins: [require("@tailwindcss/typography"),require("daisyui")],
 	daisyui: {
-		themes:[{
-			autumn: {
-			  ...require("daisyui/src/theming/themes")["autumn"],
-			  neutral: "#91707b"
-			}}],
 		themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
 		darkTheme: "dark", // name of one of the included themes for dark mode
 		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
